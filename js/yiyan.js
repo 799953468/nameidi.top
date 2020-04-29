329c0a61d4523954c8d08604b6b8247b52b833bf
799953468 <799953468@qq.com>
2020-04-29 16:38:10 +0800

    V 0.0.6

--- js/yiyan.js
+++ js/yiyan.js
@@ -4,7 +4,7 @@ window.onload=function () {
     update();
     function update() {
         gethi = new XMLHttpRequest();
-        gethi.open("GET","https://v1.hitokoto.cn/?c=d&c=i");
+        gethi.open("GET","https://v1.hitokoto.cn/?c=d&c=l&c=j");
         //这里选择类别，详见官方文档
         gethi.send();
         gethi.onreadystatechange = function () {
