7b852badb66d77dd4380a6fa7497f2658f8c9d2d
yaoyulun <799953468@qq.com>
2021-06-01 09:42:38 +0800

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,42 @@
-var gulp = require('gulp');
-var minifycss = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-var htmlmin = require('gulp-htmlmin');
-var htmlclean = require('gulp-htmlclean');
-var imagemin = require('gulp-imagemin');
+var gulp = require("gulp");
+var minifycss = require("gulp-clean-css");
+var uglify = require("gulp-uglify");
+var htmlmin = require("gulp-htmlmin");
+var htmlclean = require("gulp-htmlclean");
+const sourcemaps = require("gulp-sourcemaps");
 
 // 压缩html
-gulp.task('minify-html', function () {
-    return gulp.src('./*.html')
-        .pipe(htmlclean())
-        .pipe(htmlmin({
-            removeComments: true,
-            minifyJS: true,
-            minifyCSS: true,
-            minifyURLs: true,
-        }))
-        .pipe(gulp.dest('./public'))
+gulp.task("minify-html", function () {
+  return gulp
+    .src("./*.html")
+    .pipe(htmlclean())
+    .pipe(
+      htmlmin({
+        removeComments: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      })
+    )
+    .pipe(gulp.dest("./public"));
 });
 // 压缩css
-gulp.task('minify-css', function () {
-    return gulp.src('./css/*.css')
-        .pipe(minifycss({
-            compatibility: 'ie8'
-        }))
-        .pipe(gulp.dest('./public/css'));
+gulp.task("minify-css", function () {
+  return gulp
+    .src("./css/*.css")
+    .pipe(sourcemaps.init())
+    .pipe(minifycss())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./public/css"));
 });
 // 压缩js
-gulp.task('minify-js', function () {
-    return gulp.src('./js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./public/js'));
-});
-// 压缩图片
-gulp.task('minify-images', function () {
-    return gulp.src('./images/*.*')
-        .pipe(gulp.dest('./public/images'))
+gulp.task("minify-js", function () {
+  return gulp
+    .src("./js/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./public/js"));
 });
 // 默认任务
-gulp.task('default', gulp.series(['minify-html', 'minify-css', 'minify-js', 'minify-images']));
+gulp.task("default", gulp.series(["minify-html", "minify-css", "minify-js"]));
